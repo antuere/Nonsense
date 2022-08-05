e75fb0f4193d83d8811484ac0f7d93ca6be850d2
antuere <antuere@yandex.ru>
2022-08-05 14:06:40 +0300

    Change FindPrefix

--- src/main/java/Code/Helpful/FindPrefix.java
+++ src/main/java/Code/Helpful/FindPrefix.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class FindPrefix {
     public static void main(String[] args) {
 
-        String[] words = {"Antonsad", "Antona", "Antos", "Antrn"};
+        String[] words = {"Antonchic", "Antona", "Antos", "Antorn"};
 
 
         String result = findPrefix(words);
@@ -20,27 +20,19 @@ public class FindPrefix {
     public static String findPrefix(String[] strings) {
 
         StringBuilder result = new StringBuilder();
-        char[][] matrix = new char[strings.length][10];
 
         int countRepeat = 1;
 
-        for (int i = 0; i < strings.length; i++) {
-            String currentWord = strings[i];
-            for (int j = 0; j < currentWord.length(); j++) {
-                matrix[i][j] = strings[i].charAt(j);
-            }
-        }
-
-        for (int i = 0; i < matrix[0].length; i++) {
-            for (int j = 0, k = 1; k < matrix.length; j++, k++) {
-                if (matrix[j][i] == matrix[k][i]) {
+        for (int i = 0; i < strings[0].length(); i++) {
+            for (int j = 0, k = 1; k < strings.length; j++, k++) {
+                if (strings[j].charAt(i) == strings[k].charAt(i)) {
                     countRepeat++;
                 } else {
                     break;
                 }
             }
-            if (countRepeat == matrix.length) {
-                result.append(matrix[0][i]);
+            if (countRepeat == strings.length) {
+                result.append(strings[0].charAt(i));
                 countRepeat = 1;
             } else {
                 break;
@@ -52,21 +44,23 @@ public class FindPrefix {
 
     public static String findPrefixNew(String[] strings) {
 
-        StringBuilder result = new StringBuilder();
-
-
-        for (int j = 0; j < strings[0].length(); j++) {
-            char currentChar = strings[0].charAt(j);
-
-            for (int i = 1; i < strings.length; i++) {
-                if (currentChar != strings[i].charAt(j) && strings[i].length() <= j) {
-                    return result.toString();
-                }
-            }
-
-            result.append(currentChar);
-        }
-
-        return result.toString();
+//        StringBuilder result = new StringBuilder();
+//
+//
+//        for (int j = 0; j < strings[0].length(); j++) {
+//            char currentChar = strings[0].charAt(j);
+//
+//            for (int i = 1; i < strings.length; i++) {
+//                if (currentChar != strings[i].charAt(j) && strings[i].length() <= j) {
+//                    return result.toString();
+//                }
+//            }
+//
+//            result.append(currentChar);
+//        }
+//
+//        return result.toString();
+//    }
+        return null;
     }
 }
